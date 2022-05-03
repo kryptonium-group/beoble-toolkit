8a9050c1f4fcaabb3d46b25343d58df49940d5b0
Jungwoo Yun <bamnenim@gmail.com>
2022-05-04 06:55:39 +0900

    story book configure for all tsx

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
     ...rootMain.stories,
     '../src/lib/**/*.stories.mdx',
     '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/*stories.@(tsx)',
   ],
   addons: [...rootMain.addons, '@nrwl/react/plugins/storybook'],
   webpackFinal: async (config, { configType }) => {
